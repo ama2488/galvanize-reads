75cdc4fc324956a819969eaa807724700f82e6e1
Amanda Allen <amanda.allen2488@gmail.com>
2017-06-16 14:33:39 -0500

    initial deployment

--- routes/authors.js
+++ routes/authors.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 
 router.get('/authors', (req, res, next) => {
   db.getAuthorsWithBooks().then((result) => {
-    // console.log(knex);
+    console.log('current env:', env);
     res.render('authors', { title: 'Authors', authors: result });
   })
   .catch((err) => {
